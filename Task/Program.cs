88e74d9ed17e9e14181c7971ea1b98c9ed368beb
Igor Panfilov <i26p@ya.ru>
2023-03-26 17:21:59 +0300

    Task

---
+++ Task/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using static System.Console;
+Clear();
+string[]array = {"Hello", "2", "World", "Russia", "-3"};
+Write($"[{String.Join(", ",array)}]");
+int n = new Random().Next(4);
+string[]result = new string[n];
+for (int i = 0; i < n; i++)
+result[i]=array[i];
+Write($" -> [{String.Join(", ",result)}]");
